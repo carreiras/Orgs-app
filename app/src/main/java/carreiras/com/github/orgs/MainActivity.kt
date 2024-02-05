c231e3b81f96898a8c652f075e292d378d70e6ae
Ewerton Carreira <ewertoncarreira@gmail.com>
2024-02-05 15:21:19 -0300

    Adcionando views na Activity

--- app/src/main/java/carreiras/com/github/orgs/MainActivity.kt
+++ app/src/main/java/carreiras/com/github/orgs/MainActivity.kt
@@ -1,6 +1,16 @@
 package carreiras.com.github.orgs
 
 import android.app.Activity
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 
 class MainActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val view = TextView(this)
+        view.setText("cesta de frutas")
+        setContentView(view)
+    }
 }
