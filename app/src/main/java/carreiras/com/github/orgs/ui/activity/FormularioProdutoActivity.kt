463c4334182849f69c278b068e2dcee15be91bba
Ewerton Carreira <ewertoncarreira@gmail.com>
2024-02-14 16:50:10 -0300

    Criando a Activity com o AS

---
+++ app/src/main/java/carreiras/com/github/orgs/ui/activity/FormularioProdutoActivity.kt
@@ -0,0 +1,12 @@
+package carreiras.com.github.orgs.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import carreiras.com.github.orgs.R
+
+class FormularioProdutoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_formulario_produto)
+    }
+}
