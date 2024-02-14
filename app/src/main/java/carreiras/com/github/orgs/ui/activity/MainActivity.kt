d0bf731e62e7fd142d994c2da7abc92cda863f66
Ewerton Carreira <ewertoncarreira@gmail.com>
2024-02-14 16:15:10 -0300

    Finalizando a implementação do adapter

--- app/src/main/java/carreiras/com/github/orgs/ui/activity/MainActivity.kt
+++ app/src/main/java/carreiras/com/github/orgs/ui/activity/MainActivity.kt
@@ -2,9 +2,12 @@ package carreiras.com.github.orgs.ui.activity
 
 import android.app.Activity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import carreiras.com.github.orgs.R
+import carreiras.com.github.orgs.model.Produto
 import carreiras.com.github.orgs.ui.recyclerview.adapter.ListaProdutosAdapter
+import java.math.BigDecimal
 
 class MainActivity : Activity() {
 
@@ -14,16 +17,23 @@ class MainActivity : Activity() {
 
         // Binding: processo de vinculação do código fonte com o arquivo de layout.
 //        val nome = findViewById<TextView>(R.id.nome)
-//        nome.setText("Cesta de frutas")
+//        nome.setText("Cesta de frutas") ou
 //        nome.text = "Cesta de frutas"
-
 //        val descricao = findViewById<TextView>(R.id.descricao)
 //        descricao.text = "Laranja, manga e uva"
-
 //        val valor = findViewById<TextView>(R.id.valor)
 //        valor.text = "19.99"
 
         val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
-        recyclerView.adapter = ListaProdutosAdapter()
+        recyclerView.adapter = ListaProdutosAdapter(
+            context = this,
+            produtos = listOf(
+                Produto("Teste 0", "Teste Descrição 0", BigDecimal("19.90")),
+                Produto("Teste 1", "Teste Descrição 1", BigDecimal("29.90")),
+                Produto("Teste 2", "Teste Descrição 2", BigDecimal("39.90")),
+            )
+        )
+        // Pode ser configurado no layout
+//        recyclerView.layoutManager = LinearLayoutManager(this)
     }
 }
--- app/src/main/java/carreiras/com/github/orgs/ui/recyclerview/adapter/ListaProdutosAdapter.kt
+++ app/src/main/java/carreiras/com/github/orgs/ui/recyclerview/adapter/ListaProdutosAdapter.kt
@@ -1,23 +1,46 @@
 package carreiras.com.github.orgs.ui.recyclerview.adapter
 
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import carreiras.com.github.orgs.R
 import carreiras.com.github.orgs.model.Produto
 
-class ListaProdutosAdapter(private val produtos: List<Produto>) :
-    RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+class ListaProdutosAdapter(
+    private val context: Context,
+    private val produtos: List<Produto>
+) : RecyclerView.Adapter<ListaProdutosAdapter.ViewHolder>() {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        TODO("Not yet implemented")
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+        fun vincula(produto: Produto) {
+            val nome = itemView.findViewById<TextView>(R.id.nome)
+            nome.text = produto.nome
+
+            val descricao = itemView.findViewById<TextView>(R.id.descricao)
+            descricao.text = produto.descricao
+
+            val valor = itemView.findViewById<TextView>(R.id.valor)
+            valor.text = produto.valor.toPlainString()
+        }
     }
 
-//    override fun getItemCount(): Int {
-//        return produtos.size
-//    }
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): ListaProdutosAdapter.ViewHolder {
+        val inflater = LayoutInflater.from(context)
+        val view = inflater.inflate(R.layout.produto_item, parent, false)
+        return ListaProdutosAdapter.ViewHolder(view)
+    }
 
     override fun getItemCount(): Int = produtos.size
 
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+    override fun onBindViewHolder(holder: ListaProdutosAdapter.ViewHolder, position: Int) {
+        val produto = produtos[position]
+        holder.vincula(produto)
     }
 }
