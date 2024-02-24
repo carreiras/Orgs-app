af5cadf33c6ee025b9981a5875c10600a443a708
Ewerton Carreira <ewertoncarreira@gmail.com>
2024-02-24 10:47:43 -0300

    Configurando o listener de clique #1

--- app/src/main/java/carreiras/com/github/orgs/ui/activity/FormularioProdutoActivity.kt
+++ app/src/main/java/carreiras/com/github/orgs/ui/activity/FormularioProdutoActivity.kt
@@ -1,7 +1,9 @@
 package carreiras.com.github.orgs.ui.activity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
 import carreiras.com.github.orgs.R
 
 class FormularioProdutoActivity : AppCompatActivity(R.layout.activity_formulario_produto) {
@@ -16,4 +18,12 @@ class FormularioProdutoActivity : AppCompatActivity(R.layout.activity_formulario
      *      setContentView(R.layout.activity_formulario_produto)
      *  }
      */
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val campoNome = findViewById<EditText>(R.id.nome)
+        val nome = campoNome.text.toString()
+        Log.i("FormularioProduto", "onCreate: $nome")
+    }
 }
