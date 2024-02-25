176fb2117627fb158a9bb65695ea80e02f9da568
Ewerton Carreira <ewertoncarreira@gmail.com>
2024-02-25 15:46:21 -0300

    Configurando o listener de clique #2

--- app/src/main/java/carreiras/com/github/orgs/ui/activity/FormularioProdutoActivity.kt
+++ app/src/main/java/carreiras/com/github/orgs/ui/activity/FormularioProdutoActivity.kt
@@ -2,6 +2,8 @@ package carreiras.com.github.orgs.ui.activity
 
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.Button
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import carreiras.com.github.orgs.R
@@ -22,8 +24,14 @@ class FormularioProdutoActivity : AppCompatActivity(R.layout.activity_formulario
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val campoNome = findViewById<EditText>(R.id.nome)
-        val nome = campoNome.text.toString()
-        Log.i("FormularioProduto", "onCreate: $nome")
+        val botaoSalvar = findViewById<Button>(R.id.botao_salvar)
+
+        botaoSalvar.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                val campoNome = findViewById<EditText>(R.id.nome)
+                val nome = campoNome.text.toString()
+                Log.i("FormularioProduto", "onCreate: $nome")
+            }
+        })
     }
 }
