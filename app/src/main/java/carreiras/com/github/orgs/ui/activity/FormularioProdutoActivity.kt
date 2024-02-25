07a5428b8747a0e8bd8c7f49d8e779307e6dc628
Ewerton Carreira <ewertoncarreira@gmail.com>
2024-02-25 15:47:42 -0300

    Configurando o listener de clique #3 - Convert to lambda

--- app/src/main/java/carreiras/com/github/orgs/ui/activity/FormularioProdutoActivity.kt
+++ app/src/main/java/carreiras/com/github/orgs/ui/activity/FormularioProdutoActivity.kt
@@ -10,28 +10,15 @@ import carreiras.com.github.orgs.R
 
 class FormularioProdutoActivity : AppCompatActivity(R.layout.activity_formulario_produto) {
 
-    /**
-     * Com AppCompatActivity, podemos retirar toda a parte do setContentView
-     * e até mesmo o método onCreate e colocar a referência ao layout no
-     * contrutor da classe
-     *
-     *  override fun onCreate(savedInstanceState: Bundle?) {
-     *      super.onCreate(savedInstanceState)
-     *      setContentView(R.layout.activity_formulario_produto)
-     *  }
-     */
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         val botaoSalvar = findViewById<Button>(R.id.botao_salvar)
 
-        botaoSalvar.setOnClickListener(object : View.OnClickListener {
-            override fun onClick(v: View?) {
-                val campoNome = findViewById<EditText>(R.id.nome)
-                val nome = campoNome.text.toString()
-                Log.i("FormularioProduto", "onCreate: $nome")
-            }
-        })
+        botaoSalvar.setOnClickListener {
+            val campoNome = findViewById<EditText>(R.id.nome)
+            val nome = campoNome.text.toString()
+            Log.i("FormularioProduto", "onCreate: $nome")
+        }
     }
 }
