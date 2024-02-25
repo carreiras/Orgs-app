51f1afca7bfc18126aa1799721bab48a7d0f1f32
Ewerton Carreira <ewertoncarreira@gmail.com>
2024-02-25 17:01:31 -0300

    Inicializando Activities #5 - Disparando o botão

--- app/src/main/java/carreiras/com/github/orgs/ui/activity/MainActivity.kt
+++ app/src/main/java/carreiras/com/github/orgs/ui/activity/MainActivity.kt
@@ -1,20 +1,19 @@
 package carreiras.com.github.orgs.ui.activity
 
-import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import carreiras.com.github.orgs.R
 import carreiras.com.github.orgs.model.Produto
 import carreiras.com.github.orgs.ui.recyclerview.adapter.ListaProdutosAdapter
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import java.math.BigDecimal
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_main)
 
         val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
         recyclerView.adapter = ListaProdutosAdapter(
@@ -25,7 +24,11 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
                 Produto("Teste 2", "Teste Descrição 2", BigDecimal("39.90")),
             )
         )
-        // Pode ser configurado no layout
-//        recyclerView.layoutManager = LinearLayoutManager(this)
+
+        val fab = findViewById<FloatingActionButton>(R.id.floatingActionButton)
+        fab.setOnClickListener {
+            val intent = Intent(this, FormularioProdutoActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
