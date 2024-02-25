e728cca22300ca1bbd1a7998abdc7cc6a656a1ae
Ewerton Carreira <ewertoncarreira@gmail.com>
2024-02-25 18:46:38 -0300

    Conhecendo o ciclo de vida da Activity #1 - companion object

--- app/src/main/java/carreiras/com/github/orgs/dao/ProdutosDao.kt
+++ app/src/main/java/carreiras/com/github/orgs/dao/ProdutosDao.kt
@@ -4,13 +4,15 @@ import carreiras.com.github.orgs.model.Produto
 
 class ProdutosDao {
 
-    private val produtos = mutableListOf<Produto>()
-
     fun adiciona(produto: Produto) {
-        produtos.add(produto)
+        Companion.produtos.add(produto)
     }
 
     fun buscaTodos(): List<Produto> {
-        return produtos.toList()
+        return Companion.produtos.toList()
+    }
+
+    companion object {
+        private val produtos = mutableListOf<Produto>()
     }
 }
