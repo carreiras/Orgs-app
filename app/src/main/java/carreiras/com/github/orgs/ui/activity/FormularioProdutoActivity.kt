9f8287d9264da50ca1feee7184544624f78178ce
Ewerton Carreira <ewertoncarreira@gmail.com>
2024-02-25 16:10:20 -0300

    Criando o produto no formulário #1 - Quebra por causa do valor como texto

--- app/src/main/java/carreiras/com/github/orgs/ui/activity/FormularioProdutoActivity.kt
+++ app/src/main/java/carreiras/com/github/orgs/ui/activity/FormularioProdutoActivity.kt
@@ -2,11 +2,12 @@ package carreiras.com.github.orgs.ui.activity
 
 import android.os.Bundle
 import android.util.Log
-import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import carreiras.com.github.orgs.R
+import carreiras.com.github.orgs.model.Produto
+import java.math.BigDecimal
 
 class FormularioProdutoActivity : AppCompatActivity(R.layout.activity_formulario_produto) {
 
@@ -17,8 +18,20 @@ class FormularioProdutoActivity : AppCompatActivity(R.layout.activity_formulario
 
         botaoSalvar.setOnClickListener {
             val campoNome = findViewById<EditText>(R.id.nome)
+            val campoDescricao = findViewById<EditText>(R.id.descricao)
+            val campoValor = findViewById<EditText>(R.id.valor)
+
             val nome = campoNome.text.toString()
-            Log.i("FormularioProduto", "onCreate: $nome")
+            val descricao = campoDescricao.text.toString()
+            val valor = campoValor.text.toString()
+
+            val produtoNovo = Produto(
+                nome = nome,
+                descricao = descricao,
+                valor = BigDecimal(valor)
+            )
+
+            Log.i("FormularioProduto", "onCreate: $produtoNovo")
         }
     }
 }
