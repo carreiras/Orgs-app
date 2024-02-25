ef06b3279661b2c942732429e81d63c0bc30dee8
Ewerton Carreira <ewertoncarreira@gmail.com>
2024-02-25 17:18:43 -0300

    Implementando o DAO #2

--- app/src/main/java/carreiras/com/github/orgs/ui/activity/MainActivity.kt
+++ app/src/main/java/carreiras/com/github/orgs/ui/activity/MainActivity.kt
@@ -5,10 +5,9 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
 import carreiras.com.github.orgs.R
-import carreiras.com.github.orgs.model.Produto
+import carreiras.com.github.orgs.dao.ProdutosDao
 import carreiras.com.github.orgs.ui.recyclerview.adapter.ListaProdutosAdapter
 import com.google.android.material.floatingactionbutton.FloatingActionButton
-import java.math.BigDecimal
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
@@ -16,13 +15,11 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
         super.onCreate(savedInstanceState)
 
         val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
+        val dao = ProdutosDao()
+
         recyclerView.adapter = ListaProdutosAdapter(
             context = this,
-            produtos = listOf(
-                Produto("Teste 0", "Teste Descrição 0", BigDecimal("19.90")),
-                Produto("Teste 1", "Teste Descrição 1", BigDecimal("29.90")),
-                Produto("Teste 2", "Teste Descrição 2", BigDecimal("39.90")),
-            )
+            produtos = dao.buscaTodos()
         )
 
         val fab = findViewById<FloatingActionButton>(R.id.floatingActionButton)
