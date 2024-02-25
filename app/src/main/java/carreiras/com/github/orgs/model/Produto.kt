67081904cc73d2f4211a15283ee205ef87a8e171
Ewerton Carreira <ewertoncarreira@gmail.com>
2024-02-25 16:16:52 -0300

    Criando o produto no formulário #3 - Alterando produto para uma dataclass

--- app/src/main/java/carreiras/com/github/orgs/model/Produto.kt
+++ app/src/main/java/carreiras/com/github/orgs/model/Produto.kt
@@ -2,7 +2,7 @@ package carreiras.com.github.orgs.model
 
 import java.math.BigDecimal
 
-class Produto(
+data class Produto(
     val nome: String,
     val descricao: String,
     val valor: BigDecimal
